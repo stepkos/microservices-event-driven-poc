026f60b7d6bb9df896a51c0325eb7f4572c24631
stepkos <stepkos@wp.pl>
2025-03-15 02:46:38 +0100

    add acknowledgement

--- rabbitmq_tutorial/work_queues/worker.py
+++ rabbitmq_tutorial/work_queues/worker.py
@@ -17,8 +17,9 @@ def main():
         print(f" [x] Received {body.decode()}")
         time.sleep(body.count(b'.'))
         print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='hello', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
